b57f228d14d7c3ca2306d0093d6b892c58850fc5
hikari <kanbarusuruga0127@gmail.com>
2022-08-17 09:35:04 +0900

    add v-pre

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp ({
     data: ()=> ({
         message: "Hello PiyoPiyo!"
-    })
+    }),
+    methods: {
+        clickHandler: function(event) {
+            this.message = this.message.split("").reverse().join("")
+        }
+    }
 })
 app.mount("#app")
