3bd6040b394e612a8dbbe6cbfa656d19ec842d0f
hikari <kanbarusuruga0127@gmail.com>
2022-08-17 11:11:05 +0900

    delete some html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp ({
-    data: ()=> ({
-        message: 'Hello <span style="color:red;">PiyoPiyo!</span>'
+    data: () => ({
+        basePrice: 100
     }),
-    methods: {
-        clickHandler: function(event) {
-            this.message = this.message.split("").reverse().join("")
+    computed: {
+        taxIncludedPrice: {
+            get: function() {
+                return this.basePrice *1.1
+            },
+            set: function(value) {
+                this.basePrice = value /1.1
+            }
         }
     }
+    // data: ()=> ({
+    //     // message: 'HelloPiyoPiyo!',
+    //     // number: 100,
+    //     // ok: true,
+    //     // url: 'https://www.google.co.jp/'
+    // }),
+    // computed: {
+    //     reversedMessage: function() {
+    //         return this.message.split('').reverse().join('')
+    //     }
+    // },
+    // methods: {
+    //     reversedMessageMethod: function() {
+    //         return this.message.split('').reverse().join('')
+    //     }
+    // }
 })
 app.mount("#app")
